1a3c848627e331378f0dd733ce5f64d198275739
shehnaz123 <56083041+shehnaz123@users.noreply.github.com>
2019-10-05 08:12:29 +0500

    Update wp-config.php

--- shehnaz_1/wp-config.php
+++ shehnaz_1/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'shez' );
+define( 'DB_NAME', 'mhPatnY930' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', ' mhPatnY930' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'OGHZCt7EvQ' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'remotemysql.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
