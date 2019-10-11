8291ad37980f8de3eb20154fa4fe4e691fb35e2d
bisma123 <bisma.waljee@gmail.com>
2019-10-11 21:30:20 +0500

    divitheme

--- shehnaz_1/wp-config.php
+++ shehnaz_1/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'shez' );
+define( 'DB_NAME', 'jPzVIpDs1P' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'jPzVIpDs1P' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '6vevhJAI6q' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'remotemysql.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
