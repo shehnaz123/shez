585c09328d1814322b0e32b98110eed56fe550b1
shehnaz123 <56083041+shehnaz123@users.noreply.github.com>
2019-10-24 14:41:31 +0500

    sliderhomework

--- shehnaz_1/wp-content/themes/Divi-child/functions.php
+++ shehnaz_1/wp-content/themes/Divi-child/functions.php
@@ -12,3 +12,16 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// Slick slider styles
+function slick_slider_styles() {
+    wp_enqueue_style( 'slick-slider-styles', get_stylesheet_directory_uri() . '/slick/slick.min.css' ); 
+    wp_enqueue_style( 'slick-slider-theme-styles', get_stylesheet_directory_uri() . '/slick/slick-theme.min.css' );  
+}
+add_action( 'wp_enqueue_scripts', 'slick_slider_styles');
+
+// Slick slider js
+function slick_slider_js() {
+    wp_enqueue_script('slick-slider-js', get_stylesheet_directory_uri() . '/slick/slick.min.js', array('jquery'), '', true );
+}
+add_action( 'wp_enqueue_scripts', 'slick_slider_js');
